2986dd9d9a7098b4324de752984ca5dd4b0fb55e
divin_d1 <rukundof993@gmail.com>
2025-11-21 15:41:48 +0200

    Solving issue in /api/routes in returning promise

--- api/[...route].ts
+++ api/[...route].ts
@@ -2,13 +2,17 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { createServer } from "../server";
 import serverless from "serverless-http";
 
+// Create your Express app
 const app = createServer();
+
+// Wrap it with serverless-http
 const handler = serverless(app);
 
+// Export the Vercel function
 export default async function (
   req: VercelRequest,
   res: VercelResponse
-): Promise<void> {
-  return handler(req, res) as Promise<void>;
+): Promise<any> {
+  // serverless-http returns Promise<Object>, so Promise<any> is correct
+  return handler(req, res);
 }
-
